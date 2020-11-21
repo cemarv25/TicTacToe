c198cd26a23c16ce8dc1f02d0b3cf2fb7e3227db
cemarv25 <cesar.martinez0010@outlook.com>
2020-11-20 19:58:47 -0700

    Clients receive info from the other client

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ server_side_socket = socket.socket()
 host = 'localhost'
 port = 8080
 thread_count = 0
+clients = []
 
 try:
     server_side_socket.bind((host, port))
@@ -23,12 +24,23 @@ def multi_threaded_client(conn):
         print(data)
         if not data:
             break
-        conn.send(data)
+        
+        #Check which client we have
+        if conn == clients[0]:
+            clients[1].send(data)
+        else:
+            clients[0].send(data)
+
+        #conn.send(data)
+    if conn in clients:
+        conn_index = clients.index(conn)
+        clients.remove(conn_index)
     conn.close()
 
 
 while True:
     client, add = server_side_socket.accept()
+    clients.append(client)
     print('Connected to: ' + add[0] + ':' + str(add[1]))
     start_new_thread(multi_threaded_client, (client,))
     thread_count += 1
